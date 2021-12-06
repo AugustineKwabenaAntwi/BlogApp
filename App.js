5937446254dfa045771bc94ce8e16ccc3ac2a852
Augustine Kwabena Antwi <antwigoodness@gmail.com>
2021-12-06 22:59:23 +0000

    ----

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
+import BlogHomeScreen from './components/BlogHomeScreen';
+import BlogList from './components/BlogList';
+import BlogPostScreen from './components/BlogPostScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack =createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      </Stack.Navigator>
+     
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:100
   },
 });
